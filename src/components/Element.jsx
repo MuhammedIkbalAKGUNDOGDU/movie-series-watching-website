70722212a19fede9fb22317ff1d7cfce0b255013
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2024-07-05 10:30:50 +0300

    Movies and tvseries added to pages

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Movie from "../assets/movies.svg";
 
-const Element = () => {
+const Element = ({year,type,name,attention,splashart}) => {
   return (
-    <div>Element</div>
-  )
-}
+    <div className="element-container">
+      <img src={splashart} alt="Element Image" />
+      <div className="element-properties">
+            <text>{year}</text>
+            <text>*</text>
+            <img src={Movie} alt="" />
+            <text>{type}</text>
+            <text>*</text>
+            <text>{attention}</text>            
+      </div>
+      <div className="element-name">
+            {name}
+      </div>
+    </div>
+  );
+};
 
-export default Element
+export default Element;
--- src/components/ElementShow.jsx
+++ src/components/ElementShow.jsx
@@ -1,26 +1,83 @@
-import React from 'react'
+import React from "react";
 import Element from "../components/Element";
 
-const Recommended = ({Title}) => {
-  
-  const fetchData = () => {
+const Recommended = ({ Title }) => {
+  const imageUrl = "https://via.placeholder.com/150"; // Örneğin Splashart'tan alınan link
 
-  
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=365a153ca89400a4c6bd390fbbe93f59')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+  const fetchData = () => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=365a153ca89400a4c6bd390fbbe93f59"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
     console.log(data);
-  }
+  };
 
   return (
-    <div className='container-trends'>
-        <div className='trend-title'>
-            {Title}
-        </div>
-        <Element/>
+    <div className="container-trends">
+      <div className="trend-title">{Title}</div>
+      <div className="element-box">
+        <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+        <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+         <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+         <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+         <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+         <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+         <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+         <Element
+          year="2023"
+          type="Movie"
+          name="Example Movie"
+          attention="High"
+          splashart={imageUrl}
+        />
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Recommended
+export default Recommended;
